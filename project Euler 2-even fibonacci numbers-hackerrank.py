a92c9e8704d8dd142b599f60ad96fd60e91264be
Juan Cruz Emmanuel Noya <juancruznoya@mi.unc.edu.ar>
2021-09-01 04:48:07 -0300

    Add files via upload

---
+++ project Euler 2-even fibonacci numbers-hackerrank.py
@@ -0,0 +1,17 @@
+
+# "Project Euler #2: Even Fibonacci numbers"   
+
+def even_fibonacci_sum(n):
+    fn_2 = 1 #Fn-2
+    fn_1 = 1 #Fn-1
+    sum = 0
+    while True :
+        fn = fn_2 + fn_1 #Fn
+        if fn >= n: return sum
+        if fn % 2 == 0: sum += fn
+        fn_2, fn_1 = fn_1, fn
+
+t = int(input().strip())
+for i in range(t):
+    n = int(input().strip())
+    print(even_fibonacci_sum(n))
